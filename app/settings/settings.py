1b92be30fbc5ad6cc8c7efb529b1da67d5acc232
Anoraaklenix <anoraaklenix@gmail.com>
2022-08-24 12:30:27 +0300

    not fun only class

--- app/settings/settings.py
+++ app/settings/settings.py
@@ -39,9 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'currency',
     'django_extensions',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -125,3 +128,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if DEBUG:
+    import socket
+    hostame, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
--- app/settings/urls.py
+++ app/settings/urls.py
@@ -1,26 +1,17 @@
 
 from django.contrib import admin
-from django.urls import path
-from currency.views import rate_list, contact_list, index, http_response, rate_create, rate_update, rate_details, \
-    rate_delete, source_update, source_delete, source_details, source_create, source_list
+from django.urls import path, include
+from currency import views
+
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('', index),
-    path('rate/list/', rate_list),
-    path('rate/create/', rate_create),
-    path('contactus/list/', contact_list),
-    path('response/', http_response),
-    path('rate/update/<int:rate_id>/', rate_update),
-    path('rate/details/<int:rate_id>/', rate_details),
-    path('rate/delete/<int:rate_id>/', rate_delete),
-    path('source/list/', source_list),
-    path('source/create/', source_create),
-    path('source/update/<int:source_id>/', source_update),
-    path('source/details/<int:source_id>/', source_details),
-    path('source/delete/<int:source_id>/', source_delete),
+    path('', views.IndexView.as_view()),
+    path('currency/', include('currency.urls')),
+    path('__debug__/', include('debug_toolbar.urls'))
 
 
 

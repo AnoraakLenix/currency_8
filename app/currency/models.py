aa5dcae24e9294ceb05f72e62814b2377f159f9e
Anoraaklenix <anoraaklenix@gmail.com>
2022-08-27 20:01:36 +0300

    choices,admin,hm9

--- app/currency/models.py
+++ app/currency/models.py
@@ -1,9 +1,14 @@
 from django.db import models
+from currency.model_choices import CurrencyType
 
 
 class Rate(models.Model):
-    base_currency_type = models.CharField(max_length=3)
-    currency_type = models.CharField(max_length=3)
+
+    base_currency_type = models.CharField(
+        max_length=3,
+        choices=CurrencyType.choices,
+        default=CurrencyType.CURRENCY_TYPE_USD)
+    currency_type = models.CharField(max_length=3, choices=CurrencyType.choices)
     sale = models.DecimalField(max_digits=10, decimal_places=4)
     buy = models.DecimalField(max_digits=10, decimal_places=4)
     created = models.DateTimeField(auto_now_add=True)
